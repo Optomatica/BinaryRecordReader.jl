4c0f5c462e7105a22f9a9afce6d0862e66c4b433
mbeltagy <mohammed.elbeltagy@gmail.com>
2020-11-06 20:04:07 +0100

    Cleaner contruction via function

--- src/BinaryRecReader.jl
+++ src/BinaryRecReader.jl
@@ -5,12 +5,20 @@ using MacroTools
 export @construct_reader
 
 macro construct_reader(Struct, dims)
+    array_specs = eval(dims)
+    s = eval(:($__module__.$Struct))
+    esc(construct_reader_exp(Struct,s,array_specs))
+end
+
+
+macro construct_reader(Struct)
+    :(@construct_reader $Struct NamedTuple())
+end
+
+function construct_reader_exp(Struct, s, array_specs)
     expr = quote
         function Base.read(io::IOStream, ::Type{$Struct})
-        end
-    end|> prettify
-    array_specs=eval(dims)
-    s = eval(Struct)
+    end end |> prettify
     for (f,t) in  zip(fieldnames(s), fieldtypes(s))
         if haskey(array_specs,f)
             push!(expr.args[2].args, :($f = read!(io,$t(undef, $(array_specs[f])))))
@@ -21,11 +29,6 @@ macro construct_reader(Struct, dims)
     fields = [f for f in fieldnames(s)]
     push!(expr.args[2].args, Expr(:call,Struct,fields...))
     expr
-end
-
-
-macro construct_reader(Struct)
-    :(@construct_reader $Struct NamedTuple())
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
 using BinaryRecReader
 using Test
 
-@testset "BinaryRecReader.jl" begin
+# First we declare some simple structures 
+struct Boo
+    x::Int64
+    y::Float16
+end
+
+simple_file=tempname()
+open(simple_file,"w") do io
+    for i=1:1000
+        if i==500 #Special write
+            write(io,123,Float64(123))
+        else
+            write(io,rand(Int64),rand(Float16))
+        end
+    end
+end
+
+@testset "Simple Read" begin
     # Write your tests here.
 end
