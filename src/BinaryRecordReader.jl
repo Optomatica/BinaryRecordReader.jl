0eeda1177bf77da4419814122fa3f6599d8788e5
mbeltagy <mohammed.elbeltagy@gmail.com>
2020-11-29 00:42:16 +0200

    Shadowing of older readers construct_reader_deep

--- src/BinaryRecordReader.jl
+++ src/BinaryRecordReader.jl
@@ -4,12 +4,12 @@ using MacroTools
 
 export @construct_reader, construct_reader_deep, construct_reader_shallow
 
-global const def_reads=Set{DataType}()
+global const def_reads = Set{DataType}()
 
 macro construct_reader(Struct, dims)
     array_specs = eval(dims)
     s = eval(:($__module__.$Struct))
-    esc(construct_reader_exp(s,array_specs))
+    esc(construct_reader_exp(s, array_specs))
 end
 
 
@@ -39,32 +39,38 @@ function construct_reader_exp(s, array_specs)
     isbitstype(s) && (@warn("Attempt to create a reader for plain data type $s"); return nothing) # short ciruite for isbitstype
     expr = quote
         function Base.read(io::IOStream, ::Type{$s})
-    end end |> prettify
-    for (f,t) in  zip(fieldnames(s), fieldtypes(s))
-        if haskey(array_specs,f)
-            t <: Array || throw(TypeError(:construct_reader_exp, "construction of intenral reader", Array,t))
+        end end |> prettify
+    for (f, t) in  zip(fieldnames(s), fieldtypes(s))
+        if haskey(array_specs, f)
+            t <: Array || throw(TypeError(:construct_reader_exp, "construction of intenral reader", Array, t))
             et = eltype(t)
             isbitstype(et) || et ∈ def_reads || throw(ArgumentError("Array element $et has no properly defined read function"))
-            push!(expr.args[2].args, :($f = read!(io,$t(undef, $(array_specs[f])))))
+            push!(expr.args[2].args, :($f = read!(io, $t(undef, $(array_specs[f])))))
         else
             isbitstype(t) || t ∈ def_reads || throw(ArgumentError("$t has no properly defined read function"))
             push!(expr.args[2].args, :($f = read(io, $t)))
         end
     end
     fields = [f for f in fieldnames(s)]
-    push!(expr.args[2].args, Expr(:call,s,fields...))
-    push!(def_reads,s) #marking it as defined
+    push!(expr.args[2].args, Expr(:call, s, fields...))
+    push!(def_reads, s) # marking it as defined
     expr
 end
 
 function strip_array_specs(s, array_specs)
     a = Dict{Symbol,Tuple}()
+    b = Vector{Symbol}()
     for k in keys(array_specs)
-        parts=split(string(k),"__")
-        length(parts) == 1 && (a[k] = array_specs[k])
-        parts[1] == string(s) && (a[Symbol(parts[2])] = array_specs[k])
+        parts = split(string(k), "__")
+        if length(parts) == 1 
+            a[k] = array_specs[k]
+        elseif parts[1] == string(s)  
+            a[Symbol(parts[2])] = array_specs[k]
+        else 
+            push!(b, Symbol(parts[1]))
+        end
     end
-    a
+    a, b
 end
 
 """
@@ -114,7 +120,7 @@ julia> struct A
             data::Matrix{SimpleRecMat}
         end
 
-julia> construct_reader_deep(A, (data=(5,5), B__funMat=(2,2), C_time =(3,3)))
+julia> construct_reader_deep(A, (data=(5,5), B__funMat=(2,2), C__time =(3,3)))
 
 ```
 The above will generate three `read` methods for all the three types defined. 
@@ -123,22 +129,22 @@ See also: `construct_reader_shallow`, `@construct_reader`
 """
 function construct_reader_deep(s, array_specs)
     isbitstype(s) && (@warn("Attempt to create a reader for plain data type $s"); return nothing) # short ciruite for isbitstype
-    a = strip_array_specs(s, array_specs)
-    for (f,t) in  zip(fieldnames(s), fieldtypes(s))
-        if haskey(a,f)
-            t <: Array || throw(TypeError(:construct_reader_exp, "construction of intenral reader", Array,t))
+    a, b = strip_array_specs(s, array_specs)
+    for (f, t) in  zip(fieldnames(s), fieldtypes(s))
+        if haskey(a, f)
+            t <: Array || throw(TypeError(:construct_reader_deep, "construction of intenral reader", Array, t))
             et = eltype(t)
-            if !isbitstype(et) && et ∉ def_reads 
-               construct_reader_deep(et,array_specs)
+            if !isbitstype(et) && (Symbol(et) ∈ b || et ∉ def_reads ) 
+                construct_reader_deep(et, array_specs)
             end
         else
             isbitstype(t) || t ∈ def_reads || t <: Array && throw(ArgumentError("$t has no properly defined read function"))
-            if !isbitstype(t) && t ∉ def_reads 
-               construct_reader_deep(t,array_specs)
+            if !isbitstype(t) && (Symbol(t) ∈ b || t ∉ def_reads )
+                construct_reader_deep(t, array_specs)
             end
         end
     end
-    eval(construct_reader_exp(s,a))
+    eval(construct_reader_exp(s, a))
 end
 
 end
